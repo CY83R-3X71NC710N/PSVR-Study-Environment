bee771b9d66dc8632c399168c87b2422e0961092
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-05-08 18:53:02 -0500

    Update studygenerate.sh

--- studygenerate.sh
+++ studygenerate.sh
@@ -12,6 +12,15 @@ ulimit -v 4000000
 # Interestingly the more I use this video editing setup the more I want to avoid GUI programs. This is faster and more efficient.
 # Using a command-line interface (CLI) instead of a graphical user interface (GUI) can provide faster and more efficient video editing capabilities.
 
+# Extract the rotation metadata from the non360.mp4 file
+rotation=$(ffprobe -v error -select_streams v:0 -show_entries metadata=s:rotate -of default=noprint_wrappers=1:nokey=1 non360.mp4)
+
+# Parse the rotation metadata to get the yaw, pitch, and roll values
+IFS=' ' read -ra rotation_array <<< "$rotation"
+yaw=${rotation_array[0]}
+pitch=${rotation_array[1]}
+roll=${rotation_array[2]}
+
 # Overlay a non-360 clip onto a 360 VR video
 ffmpeg -y -i vr.mp4 \
        -i non360.mp4 \
@@ -26,10 +35,10 @@ ffmpeg -y -i non360.mp4 \
        -metadata:s:v:0 stereo_mode=mono \
        non360_360.mp4
 
-# Overlay the 360 non-360 clip onto the 360 VR video
+# Overlay the 360 non-360 clip onto the 360 VR video, using the extracted yaw, pitch, and roll values
 ffmpeg -y -i vr.mp4 \
        -i non360_360.mp4 \
-       -filter_complex "[1:v]rotate=-yaw:ow=rotw(-yaw):oh=roth(-yaw)[tmp1];[tmp1]rotate=-pitch:ow=rotw(-pitch):oh=roth(-pitch)[tmp2];[tmp2]rotate=-roll:ow=rotw(-roll):oh=roth(-roll)[tmp3];[0:v][tmp3]overlay=x=(main_w/2)-(w/2):y=(main_h/2)-(h/2)" \
+       -filter_complex "[1:v]rotate=-$yaw:ow=rotw(-$yaw):oh=roth(-$yaw)[tmp1];[tmp1]rotate=-$pitch:ow=rotw(-$pitch):oh=roth(-$pitch)[tmp2];[tmp2]rotate=-$roll:ow=rotw(-$roll):oh=roth(-$roll)[tmp3];[0:v][tmp3]overlay=x=(main_w/2)-(w/2):y=(main_h/2)-(h/2)" \
        -c:a copy \
        output_final.mp4
 
