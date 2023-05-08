dd0f90b0a994a995117f64790d7d0c55fe2834a3
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-05-08 17:56:44 -0500

    Update studygenerate.sh

--- studygenerate.sh
+++ studygenerate.sh
@@ -4,7 +4,7 @@
 
 # Interestingly the more I use this video editing setup the more I want to avoid GUI programs. This is faster and more efficient.
 
-ffmpeg -stream-loop -i vr.mp4 -i test.mp4 -filter_complex "[1]scale=iw/5:ih/5 [pip]; [0][pip] overlay=main_w-overlay_w-10:main_h-overlay_h-10" -profile:v main -level 3.1 -b:v 440k -ar 44100 -ab 128k -s 1920x1080 -vcodec h264 -acodec aac output.mp4
+ffmpeg -stream_loop -i vr.mp4 -i test.mp4 -filter_complex "[1]scale=iw/5:ih/5 [pip]; [0][pip] overlay=main_w-overlay_w-10:main_h-overlay_h-10" -profile:v main -level 3.1 -b:v 440k -ar 44100 -ab 128k -s 1920x1080 -vcodec h264 -acodec aac output.mp4
 # Make sure that the second clip and the first clip match length or make sure that the overlay loops and the first clip is longer than the second clip.
 
 # Let's test this now on my PSVR using the overlay to a vr file
