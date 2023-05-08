cb687452a3695efe6f699f17f0aeb88c6486d138
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-05-08 17:37:19 -0500

    Update studygenerate.sh

--- studygenerate.sh
+++ studygenerate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# We are now using bash due to the fact it is better suited to this task than python.
+# We are now using bash withg ffmpeg due to the fact it is better suited to this task than python.
 ffmpeg -i vr.mp4 -i test.mp4 \
 -filter_complex "[1:v]setpts=PTS-10/TB[a]; \
                  [0:v][a]overlay=enable=gte(t\,5):shortest=1[out]" \
