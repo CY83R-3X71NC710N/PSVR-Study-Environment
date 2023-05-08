bac825666371aa3b1d9b750e471cb78386bc8905
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-05-08 17:50:44 -0500

    Update studygenerate.sh

--- studygenerate.sh
+++ studygenerate.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # We are now using bash withg ffmpeg due to the fact it is better suited to this task than python.
+
+# Interestingly the more I use this video editing setup the more I want to avoid GUI programs. This is faster and more efficient.
+
 ffmpeg -i vr.mp4 -i test.mp4 -filter_complex "[1]scale=iw/5:ih/5 [pip]; [0][pip] overlay=main_w-overlay_w-10:main_h-overlay_h-10" -profile:v main -level 3.1 -b:v 440k -ar 44100 -ab 128k -s 720x400 -vcodec h264 -acodec aac output.mp4
