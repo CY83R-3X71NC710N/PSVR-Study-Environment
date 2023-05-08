457fc19a6de0bc2d2ff83f02dd3a89243a1aba1b
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-05-08 17:44:50 -0500

    Update studygenerate.sh

--- studygenerate.sh
+++ studygenerate.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 # We are now using bash withg ffmpeg due to the fact it is better suited to this task than python.
-ffmpeg -i vr.mp4 -i test.mp4 \
--filter_complex "[1:v]setpts=PTS-10/TB[a]; \
-                 [0:v][a]overlay=enable=gte(t\,5):shortest=1[out]" \
--map [out] -map 0:a \
--c:v libx264 -crf 18 -pix_fmt yuv420p \
--c:a copy \
-output.mp4
+ffmpeg -i vr.mp4 -i test.mp4 -filter_complex "[1]scale=iw/5:ih/5 [pip]; [0][pip] overlay=main_w-overlay_w-10:main_h-overlay_h-10" -profile:v main -level 3.1 -b:v 440k -ar 44100 -ab 128k -s 720x400 -vcodec h264 -acodec libfaac output.mp4
