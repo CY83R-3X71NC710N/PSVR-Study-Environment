9a7de73a9601ba0fdc63d2048861fe2041ea3266
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-05-08 18:41:57 -0500

    Update studygenerate.sh

--- studygenerate.sh
+++ studygenerate.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Set CPU priority to very low
+renice +19 $$ > /dev/null
+
+# Limit memory usage to 4GB
+ulimit -v 4000000
+
 # We are now using bash with ffmpeg due to the fact it is better suited to this task than python.
 # Bash is a command line interpreter that is commonly used in Linux and other Unix-like operating systems.
 
@@ -11,16 +17,6 @@ ffmpeg -y -i vr.mp4 \
        -i non360.mp4 \
        -filter_complex "[1:v]scale=250:-1[tmp];[0][tmp]overlay=shortest=1:x=(main_w/2)-(w/2):y=(main_h/2)-(h/2)" \
        -c:a copy \
-       -cpu-used 0 \
-       -threads 1 \
-       -strict -2 \
-       -preset veryslow \
-       -movflags +faststart \
-       -maxrate 4M \
-       -bufsize 4M \
-       -rtbufsize 4M \
-       -f mp4 \
-       -fs 4000M \
        output_overlay.mp4
 
 # Convert the non-360 clip to a 360 format
@@ -28,16 +24,6 @@ ffmpeg -y -i non360.mp4 \
        -c copy \
        -metadata:s:v:0 projection=spherical \
        -metadata:s:v:0 stereo_mode=mono \
-       -cpu-used 0 \
-       -threads 1 \
-       -strict -2 \
-       -preset veryslow \
-       -movflags +faststart \
-       -maxrate 4M \
-       -bufsize 4M \
-       -rtbufsize 4M \
-       -f mp4 \
-       -fs 4000M \
        non360_360.mp4
 
 # Overlay the 360 non-360 clip onto the 360 VR video
@@ -45,16 +31,6 @@ ffmpeg -y -i vr.mp4 \
        -i non360_360.mp4 \
        -filter_complex "[1:v]rotate=-yaw:ow=rotw(-yaw):oh=roth(-yaw)[tmp1];[tmp1]rotate=-pitch:ow=rotw(-pitch):oh=roth(-pitch)[tmp2];[tmp2]rotate=-roll:ow=rotw(-roll):oh=roth(-roll)[tmp3];[0:v][tmp3]overlay=x=(main_w/2)-(w/2):y=(main_h/2)-(h/2)" \
        -c:a copy \
-       -cpu-used 0 \
-       -threads 1 \
-       -strict -2 \
-       -preset veryslow \
-       -movflags +faststart \
-       -maxrate 4M \
-       -bufsize 4M \
-       -rtbufsize 4M \
-       -f mp4 \
-       -fs 4000M \
        output_final.mp4
 
 # Let's test this now on my PSVR using the overlay to a VR file.
